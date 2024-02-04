b4986098381cbeab469bdbf67d05aed7571d4ea8
Yakubenka Ivan <palasja@gmail.com>
2024-02-04 16:38:35 +0300

    feat: home add top omage

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -3,9 +3,14 @@ import './home.css';
 
 const Home = () => {
   return (
-    <>
-      <TopPanel />
-    </>
+    <section className="home-top-panel">
+      <div className="home-top_text text">
+        <h2 className='text_service-name'>Парикхмахерские услуги</h2>
+        <h1 className='text_company-name'>New Style</h1>
+        <a href='' className='text_cost-link'>Услуги и Цены</a>
+      </div>
+      <img src={new URL(`../../assets/images/pattran.png`, import.meta.url).href} alt="pattran" />
+    </section>
   );
 };
 
