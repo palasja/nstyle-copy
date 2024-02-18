5bf70e3eb74a6d0ca82a4076988c9adac5c59a78
Yakubenka Ivan <palasja@gmail.com>
2024-02-18 14:24:43 +0300

    feat: add promo info om homePage

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,14 +2,20 @@ import './home.css';
 import HomeSlider from '../../components/homeSlider';
 import ServiceNavigation from '../../components/serviceNavigation';
 import ServicesCost from '../../components/servicesCost';
+import SocialMediaLink from '../../components/socialMediaLink';
 
 const Home = () => {
-  // const [sliderImageInfo, setSliderImageInfo] = useState<SliderImageProps>(map.get('haidress') as SliderImageProps);
   return (
     <div>
       <HomeSlider />
       <ServiceNavigation />
       <ServicesCost />
+      <section className='promo-info'>
+        <h3 className='promo-info_style'>СЛЕДИ ЗА СТИЛЕМ!</h3>
+        <h2 className='promo-info_anonce'>Еженедельные анонсы</h2>
+        <p className='promo-info_social'>На сайте и в социальных сетях</p>
+        <SocialMediaLink />
+      </section>
     </div>
   );
 };
